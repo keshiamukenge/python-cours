16498941aad700b16f18f956116869b2c04b8b14
keshiamukenge <mukenge.keshia@gmail.com>
2022-06-09 17:29:00 +0200

    init

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-5&+1^%hg-h9p(1sgp9l5lkrgr=)m^!_f05rw%n_w1&&gvjyx*f"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
